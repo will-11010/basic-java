69aabecb9bd7bbc9b084acbc6cd3c6b0bcaf5bc5
will_11010 <54029586+will-11010@users.noreply.github.com>
2025-10-21 01:13:03 +0000

    Substitui a entrada de dados por um menu interativo para consulta e manipulação de saldo

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String nome = "Clark Kent";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
 
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        System.out.print("Digite sua idade: ");
-        int idade = scanner.nextInt();
-        System.out.print("Digite o valor que pretende investir esse mês: ");
-        double valor = scanner.nextDouble();
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n***********************");
 
-        System.out.println(nome + " que tem " + idade + " anos, irá investir R$ " + valor + " esse mês.");
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor 
+                4 - Sair
+                
+                """;
+        Scanner leitura = new Scanner(System.in);
 
-        scanner.close();
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual o valor que deseja transferir?");
+                double valor = leitura.nextDouble();
+                if (valor > saldo) {
+                    System.out.println("Não há saldo para realizar a transferência.");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Novo saldo: " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Novo saldo: " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida!");
+            }
+        }
     }
 }
