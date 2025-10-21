53abc483e4acac37603e0b93b79382e683b8922e
will_11010 <54029586+will-11010@users.noreply.github.com>
2025-10-21 01:08:33 +0000

    Substitui a mensagem "Hello, World!" por um formulário interativo para entrada de nome, idade e valor de investimento.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        System.out.print("Digite o valor que pretende investir esse mês: ");
+        double valor = scanner.nextDouble();
+
+        System.out.println(nome + " que tem " + idade + " anos, irá investir R$ " + valor + " esse mês.");
+
+        scanner.close();
     }
 }
